6924f9b41f94169460c65fbf8c7d26fcfbab4686
zohaibdhukka <zohaibdhukka@gmail.com>
2019-03-23 16:01:47 -0500

    replace routes file to new one

---
+++ routes/bars-api-routes.js
@@ -0,0 +1,39 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+  app.get("/api/bars", function(req, res) {
+    var query = {};
+    if (req.query.cities_id) {
+      query.CityId = req.query.Cities_id;
+    }
+
+    db.Bars.findAll({
+      where: query,
+      include: [db.Cities]
+    }).then(function(dbBars) {
+      res.json(dbBars);
+    });
+  });
+
+  app.get("/api/bars/:id", function(req, res) {
+
+    db.Bars.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Cities]
+    }).then(function(dbBars) {
+      res.json(dbBars);
+    });
+  });
+
+
+  app.post("/api/bars", function(req, res) {
+    db.Bars.create(req.body).then(function(dbBars) {
+      res.json(dbBars);
+    });
+  });
+
+  
+};
